3aab418a118bc4664da62c1c4089c2ffe666750e
Ítalo <caliari.italo@proton.me>
2023-10-29 14:22:17 -0400

    .

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
-from typing import List, Union
+import json
+import os
+from typing import Dict, List, Optional
 
-from fastapi import FastAPI
+import pandas as pd
+from fastapi import Body, FastAPI, File, UploadFile, status
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: List[str] = []
+class DataModelIn(BaseModel):
+    message: str = None
+    id: str = None
+    input_data: str = None
+    result: str = None
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "message": "Foo",
+                    "id": "A very nice Item",
+                    "input_data": "35.4",
+                    "result": "3.2",
+                }
+            ]
+        }
+    }
 
 
-@app.post("/items/")
-async def create_item(item: Item) -> Item:
-    return item
+@app.post("/teste1")
+def teste1(data: DataModelIn):
+    print(dict(data))
+    return True
 
 
-@app.get("/items/")
-async def read_items() -> List[Item]:
-    return [
-        Item(name="Portal Gun", price=42.0),
-        Item(name="Plumbus", price=32.0),
-    ]
+@app.post("/teste2")
+def teste2(data: List[DataModelIn]):
+    lista = []
+    for i in data:
+        lista.append(dict(i))
+    asd = pd.DataFrame(lista)
+    print(asd)
+    return True
+
+
+if __name__ == "__main__":
+    os.system("uvicorn main:app --reload")
